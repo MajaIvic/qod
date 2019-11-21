4c2c0046acf0e2ad8aa9edbb01f8a72e76d8fe90
Maja <rojasmaja5@gmail.com>
2019-11-20 20:10:48 -0800

    styling for tablet size

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="nav-info">
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
@@ -17,6 +18,7 @@
 				</nav><!-- #site-navigation -->
 				<div class="link-to-red">
 				<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>
+</div>
 </div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
